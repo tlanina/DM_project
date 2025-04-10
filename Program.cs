73be0f10a7f6de4bb1d5c6922eb4c57247ea7940
tlanina <tlanina@kse.org.ua>
2025-04-10 19:11:33 +0300

    Вирішення проблеми комівояжера за допомогою жадібних алгоритмів

--- Program.cs
+++ Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Travelling_salesman_problem;
 
-Console.WriteLine("Hello, World!");
+using Travelling_salesman_problem;
+
+class Program
+{
+    static void PrintAdjacencyMatrix(Graph graph)
+    {
+        int n = graph.VerticesCount;
+
+        Console.WriteLine("Матриця суміжності:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                int weight = graph.GetWeight(i, j);
+                string value = weight == int.MaxValue ? "∞" : weight.ToString();
+                Console.Write($"{value,6} "); 
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        
+        int n = 20;  
+        double fully = 1.0; 
+        var graph = GraphGenerator.GenerateRandomGraph(n, fully, useMatrix: true);
+        
+        Console.WriteLine($"Кількість вершин: {graph.VerticesCount}");
+       
+        if (graph is AdjacencyMatrix)
+        {
+            Console.WriteLine($"Кількість ребер: {(graph as AdjacencyMatrix).EdgesCount()}");
+        }
+        else if (graph is AdjacencyList)
+        {
+            Console.WriteLine($"Кількість ребер: {(graph as AdjacencyList).EdgesCount()}");
+        }
+        
+        PrintAdjacencyMatrix(graph);
+        
+        var (cost, path) = TSPSolve.Solve(graph);
+        
+        Console.WriteLine("Жадібний алгоритм (найближчий сусід):");
+        Console.WriteLine(string.Join(" -> ", path));
+        Console.WriteLine($"Загальна довжина шляху: {cost:F2}");
+    }
+
+}
